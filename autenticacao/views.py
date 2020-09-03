a441948b7143b3d7f81c9ab8667ffc1de9c14dcb
Crístian Gusberti <crisgusberti@gmail.com>
2020-09-03 16:27:17 -0300

    <fix> Correção para login do admin. Agora o usuário e senha do admin está cadastrado no arquivo Json que foi adicionado ao gitignore

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -11,32 +11,46 @@ def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        if username == 'admin' and password == 'admin':
-            request.session['username'] = username
-            request.session['campus'] = 'FULL_ACCESS'
-            request.session['curso'] = 'FULL_ACCESS'
-            return redirect('/geral')
-        else:
+        #REMOVIDO PQ AGORA O ADMIN ESTÁ CADASTRADO NO ARQUIVO JSON
+        # if username == 'admin' and password == 'admin':
+        #     request.session['username'] = username
+        #     request.session['campus'] = 'FULL_ACCESS'
+        #     request.session['curso'] = 'FULL_ACCESS'
+        #     return redirect('/geral')
+        # else:
             #Verifica se é usuário autorizado na tabela
-            if username not in usuarios: 
-                messages.info(request, 'Usuário não autorizado') #sistema de mensagens do Django que retorna uma mensagem especifica
-                return redirect('login')
-            else:
-                request.session['username'] = username
+        if username not in usuarios: 
+            messages.info(request, 'Usuário não autorizado') #sistema de mensagens do Django que retorna uma mensagem especifica
+            return redirect('login')
+        #REMOVIDO POR NÃO CONSEGUIR USAR LDAP REITOIRA
+        # else:
+        #     request.session['username'] = username
+        #     request.session['campus'] = usuarios[username]['campus_id'] #acessa os dados do json lido pela função ler_usuarios_cadastrados
+        #     request.session['curso'] = usuarios[username]['curso_id']
+        
+        #ADICIONADO POR NÃO CONSEGUIR USAR LDAP REITOIRA
+        else:
+            request.session['username'] = username
+            if password == usuarios[username]['senha']:
                 request.session['campus'] = usuarios[username]['campus_id'] #acessa os dados do json lido pela função ler_usuarios_cadastrados
                 request.session['curso'] = usuarios[username]['curso_id']
-
-            #Autentica no LDAP 
-            username_ldap = 'uid=' + username + ',ou=People,dc=poa,dc=ifrs,dc=edu,dc=br'
-            try:
-                conn = ldap.initialize('ldap://ldap.poa.ifrs.edu.br')
-                conn.protocol_version = 3
-                conn.set_option(ldap.OPT_REFERRALS, 0)
-                conn.simple_bind_s(username_ldap, password)
-            except ldap.LDAPError:
+                return redirect('/geral')
+            else:
                 messages.info(request, 'Usuário ou senha incorretos')
-                return redirect('login')
-            return redirect('/geral')
+                return redirect('login') 
+
+            #REMOVIDO POR NÃO CONSEGUIR USAR LDAP REITOIRA
+            #Autentica no LDAP 
+            # username_ldap = 'uid=' + username + ',ou=People,dc=poa,dc=ifrs,dc=edu,dc=br'
+            # try:
+            #     conn = ldap.initialize('ldap://ldap.poa.ifrs.edu.br')
+            #     conn.protocol_version = 3
+            #     conn.set_option(ldap.OPT_REFERRALS, 0)
+            #     conn.simple_bind_s(username_ldap, password)
+            # except ldap.LDAPError:
+            #     messages.info(request, 'Usuário ou senha incorretos')
+            #     return redirect('login')
+            # return redirect('/geral')
     else:
         return render(request,'login.html')
 
